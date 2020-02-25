a5dd2f473e21434d586f505944c3d7b53a87be51
Leon Kang <kangleonjy@gmail.com>
2020-02-25 16:42:53 +0800

    new
    new

---
+++ 2020-02-25/ClassOutDefine/ClassOutDefine/Teacher.cpp
@@ -0,0 +1,9 @@
+//
+//  Teacher.cpp
+//  ClassOutDefine
+//
+//  Created by 吉腾蛟 on 2020/2/25.
+//  Copyright © 2020 JY. All rights reserved.
+//
+
+#include "Teacher.hpp"
---
+++ 2020-02-25/ClassOutDefine/ClassOutDefine/Teacher.hpp
@@ -0,0 +1,14 @@
+//
+//  Teacher.hpp
+//  ClassOutDefine
+//
+//  Created by 吉腾蛟 on 2020/2/25.
+//  Copyright © 2020 JY. All rights reserved.
+//
+
+#ifndef Teacher_hpp
+#define Teacher_hpp
+
+#include <stdio.h>
+
+#endif /* Teacher_hpp */
