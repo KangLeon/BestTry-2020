7a0f6aec4205aaef2e671a36cd49c92df370d9f5
JY <tengjiaoji@blackfish.cn>
2020-04-25 10:05:44 +0800

    new
    new

---
+++ 2020-4-26（骨架加载）/SkeletonViewOC/SkeletonViewOC/GradientTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  GradientTableViewCell.h
+//  SkeletonViewOC
+//
+//  Created by JY on 2020/4/24.
+//  Copyright © 2020 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GradientTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 2020-4-26（骨架加载）/SkeletonViewOC/SkeletonViewOC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  SkeletonViewOC
+//
+//  Created by JY on 2020/4/24.
+//  Copyright © 2020 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
