fe54ce43d28944c6c059b231758677a866a7a71b
JY <tengjiaoji@blackfish.cn>
2020-04-29 22:31:38 +0800

    new
    new

---
+++ 2020-4-29(SwiftUI)/Address/Address/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Address
+//
+//  Created by JY on 2020/4/29.
+//  Copyright © 2020 JY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
